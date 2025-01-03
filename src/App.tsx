9b449ead79be37f3f29dc426f4e992e748fe1985
dolnadolnadol <saiparnchollada@gmail.com>
2025-01-03 12:32:59 +0700

    feat

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <img src="loma-dev.png" width={250} />
+        <div className="text-2xl font-bold">Chollada Sae-lim</div>
+        <div className="text-2xl font-bold">Web Developer</div>
+        <div className="text-2xl font-bold">Oct 8, 2002</div>
+        <div className="text-2xl font-bold">Computer Science, KMITL</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{html,tsx,ts,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+} satisfies Config;
